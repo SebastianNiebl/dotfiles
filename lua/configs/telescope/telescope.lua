485aa28426ba5af946ba73ea50fa8147a80d2ac0
SebastianNiebl <github@DerSaege.e4ward.com>
2024-06-07 07:38:39 +0200

    added grep to telescope

--- lua/configs/telescope/telescope.lua
+++ lua/configs/telescope/telescope.lua
@@ -1,8 +1,9 @@
-return 
+return
 {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim', 
-                       'BurntSushi/ripgrep', 
-                       'nvim-telescope/telescope-fzf-native.nvim', 
-      }      
+      dependencies = { 'nvim-lua/plenary.nvim',
+                       'BurntSushi/ripgrep',
+                       'nvim-telescope/telescope-fzf-native.nvim',
+                       'nvim-telescope/telescope-live-grep-args.nvim',
+      }
 }
